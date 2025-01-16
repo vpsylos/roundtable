4f12545f0f7f30ee18854697c6210e9597083e62
Vivian <vpsylos@MB-Air-VP.local>
2025-01-15 22:54:52 -0600

    Made another edit to setup.py

--- setup.py
+++ setup.py
@@ -18,23 +18,7 @@ activate_script = f'{venv_dir}/bin/activate' if os.name == 'posix' else f'{venv_
 activate_cmd = f'source {activate_script}' if os.name == 'posix' else f'{activate_script}'
 subprocess.run(activate_cmd, shell=True)
 
-print("Installing package and dependencies...")
-setup(
-    name='roundtable',
-    version='1.0.0',
-    description='A Flask web application',
-    author='Vivian Psylos',
-    author_email='vpsylos@uchicago.edu',
-    packages=find_packages(),
-    install_requires=DEPENDENCIES,
-    extras_require={
-        'dev': ['python>=3.8', 'pip>=20.0.0', 'setuptools>=50.0.0', 'wheel>=0.35.0'],
-    },
-    entry_points={
-        'console_scripts': [
-            'roundtable=app:main',
-        ],
-    },
-)
+print("Installing dependencies...")
+subprocess.run('pip install -r requirements.txt', shell=True)
 
 print("Installation complete!")
